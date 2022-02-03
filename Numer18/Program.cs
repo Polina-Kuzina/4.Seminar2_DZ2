3719d6680db658f0bc3fced67976f73ce51c5493
Polina <payline@mail.ru>
2022-02-03 16:45:26 +0300

    Решила задачу 18

---
+++ Numer18/Program.cs
@@ -0,0 +1,9 @@
+﻿// 18. Дано число, обозначающее день недели. Выяснить, является ли номер дня недели выходным
+Console.Write("Введите номер дня недели: ");
+int a = int.Parse(Console.ReadLine());
+if(a <= 7)
+{
+    if(a > 5) Console.WriteLine("Этот день недели выходной");
+    else Console.WriteLine("Этот день недели не выходной");
+}  
+else Console.WriteLine("Такого дня недели нет");
