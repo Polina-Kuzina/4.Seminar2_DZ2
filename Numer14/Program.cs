a610b6b6c0f2ef7ea698e4b7fc3504a7f0f6700c
Polina <payline@mail.ru>
2022-02-03 16:35:29 +0300

    Решила задачу 14

---
+++ Numer14/Program.cs
@@ -0,0 +1,9 @@
+﻿// 14. Найти третью цифру числа или сообщить, что ее нет
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+if(a / 100 >= 1)
+{
+    while(a / 1000 >= 1) a = a / 10;    
+    Console.WriteLine("Третьей цифра числа " + a % 10);
+}
+else Console.WriteLine("В числе нет третьей цифры");
