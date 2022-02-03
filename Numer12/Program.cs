205ac63ab524040cf27080dcc395b29f77e74f8a
Polina <payline@mail.ru>
2022-02-03 16:13:29 +0300

    Решено задание 12

---
+++ Numer12/Program.cs
@@ -0,0 +1,6 @@
+﻿// 12. Удалить вторую цифру трехзначного числа
+Console.Write("Введите трехзначное число: ");
+int a = int.Parse(Console.ReadLine());
+int a3 = a % 10;
+int a1 = a / 100;
+Console.WriteLine("Число без второй цифры: " + a1 + a3);
