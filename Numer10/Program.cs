552ef9afa33e544b1c8a5acd8277ae436bc10ff3
Polina <payline@mail.ru>
2022-02-03 15:41:59 +0300

    Папка 10

---
+++ Numer10/Program.cs
@@ -0,0 +1,5 @@
+﻿// 10. Показать вторую цифру трехзначного числа
+Console.Write("Введите трехзначное число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Вторая цифра числа: " + a / 10 % 10);
+
